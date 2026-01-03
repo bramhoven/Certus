862954d635b99103dc61d015a2fa0c1cffd7d6de
Bram Hoven <2228410+bramhoven@users.noreply.github.com>
2026-01-03 10:01:03 +0100

    Add StrategyData

--- certus_core/src/strategy.rs
+++ certus_core/src/strategy.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use crate::{core::Order, data::MarketData};
 
 /// trait for trading strategies
@@ -13,4 +15,38 @@ pub trait Strategy {
     /// Will be called for every new bar or tick, when tick data is supplied
     /// returns a list of orders to be placed
     fn next(&mut self, market_data: MarketData) -> Vec<Order>;
+}
+
+#[derive(Debug, Default)]
+pub struct StrategyData {
+    pub max_data_back: usize,
+    pub market_data: VecDeque<MarketData>,
+}
+
+impl StrategyData {
+    pub fn new(max_data_back: usize) -> Self {
+        Self {
+            max_data_back,
+            market_data: VecDeque::new()
+        }
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.market_data.len() == self.max_data_back
+    }
+}
+
+impl Strategy for StrategyData {
+    fn init(&mut self) {}
+
+    fn update(&mut self, market_data: MarketData) {
+        self.market_data.push_front(market_data);
+        if self.market_data.len() > self.max_data_back {
+            self.market_data.truncate(self.max_data_back);
+        }
+    }
+
+    fn next(&mut self, _market_data: MarketData) -> Vec<Order> {
+        Vec::new()
+    }
 }
--- certus_example/src/strategy.rs
+++ certus_example/src/strategy.rs
@@ -1,9 +1,11 @@
+
 use certus_core::core::Order;
 use certus_core::indicator::{Indicator, MovingAverage};
-use certus_core::strategy::Strategy;
+use certus_core::strategy::{Strategy, StrategyData};
 use certus_core::data::MarketData;
 
 pub struct SimpleStrategy {
+    data: StrategyData,
     ma_slow: MovingAverage,
     ma_fast: MovingAverage,
 }
@@ -11,6 +13,7 @@ pub struct SimpleStrategy {
 impl SimpleStrategy {
     pub fn new() -> Self {
         Self {
+            data: StrategyData::new(50),
             ma_fast: MovingAverage::new(7),
             ma_slow: MovingAverage::new(21),
         }
@@ -19,15 +22,26 @@ impl SimpleStrategy {
 
 impl Strategy for SimpleStrategy {
     fn init(&mut self) {
+        self.data.init();
     }
     
     fn update(&mut self, market_data: MarketData) {
+        self.data.update(market_data);
+
         self.ma_fast.update(market_data);
         self.ma_slow.update(market_data);
     }
 
     fn next(&mut self, _market_data: MarketData) -> Vec<Order> {
-        println!("MA Fast {} - MA Slow {}", self.ma_fast.value, self.ma_slow.value);
+        if !self.data.is_ready() { return Vec::new(); }
+
+        let current_bar = match self.data.market_data.front() {
+            Some(MarketData::Bar(bar)) => bar,
+            _ => unreachable!("Expected only MarketData::Bar"),
+        };
+
+        println!("Close {} - MA Fast {} - MA Slow {}", current_bar.close, self.ma_fast.value, self.ma_slow.value);
+
         Vec::new()
     }
 }
